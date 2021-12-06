1194600166c52d6722f96ebf841c99ba66516fa4
Nicolas NARDONE <nna@protonmail.com>
2021-12-06 20:54:48 +0100

    day5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,7 @@
+#![feature(int_abs_diff)]
+
 use std::collections::HashMap;
+use std::fmt;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -26,14 +29,15 @@ fn main() {
         // add code here
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     struct Segment {
         start: Coordinate,
         end: Coordinate,
     }
+    //20687-7142=13545
 
     impl Segment {
-        fn points(&self) -> Vec<Coordinate> {
+        fn points(&self, counting_diagonals: bool) -> Vec<Coordinate> {
             let mut p = vec![];
             if self.start.row == self.end.row {
                 let range;
@@ -65,7 +69,50 @@ fn main() {
                     });
                 }
             }
+
+            if counting_diagonals && self.is_diagonal_45() {
+                let row_range;
+                let col_range;
+                // We are on a 45 diagnonal
+
+                // Determine ranges
+                if self.start.row <= self.end.row {
+                    row_range = self.start.row..=self.end.row;
+                } else {
+                    row_range = self.end.row..=self.start.row;
+                }
+                if self.start.column <= self.end.column {
+                    col_range = self.start.column..=self.end.column;
+                } else {
+                    col_range = self.end.column..=self.start.column;
+                }
+
+                if (self.start.row + self.start.column != self.end.row + self.end.column) {
+                    let min_col_range = col_range.min().unwrap();
+                    for (i, r) in row_range.enumerate() {
+                        p.push(Coordinate {
+                            row: r,
+                            column: min_col_range + i as u16,
+                        });
+                    }
+                } else {
+                    // self.start.row + self.start.column == self.end.row + self.end.column
+                    let max_col_range = col_range.max().unwrap();
+
+                    for (i, r) in row_range.enumerate() {
+                        p.push(Coordinate {
+                            row: r,
+                            column: max_col_range - i as u16,
+                        });
+                    }
+                }
+            }
+
             p
+        }
+
+        fn is_diagonal_45(&self) -> bool {
+            self.end.row.abs_diff(self.start.row) == self.end.column.abs_diff(self.start.column)
         }
     }
 
@@ -75,11 +122,6 @@ fn main() {
         }
 
         fn add_point(&mut self, coord: Coordinate) {
-            // if let Some(row) = self.content.get_mut(row_num as usize) {
-            //     if let Some(point) = row.get_mut(col_num as usize) {
-            //         *point += 1;
-            //     }
-            // }
             if let Some(value) = self.index.get_mut(&coord) {
                 *value += 1;
             } else {
@@ -98,6 +140,23 @@ fn main() {
                 index: HashMap::new(),
                 content: vec![vec![]],
             }
+        }
+    }
+
+    impl fmt::Display for Grid {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let mut grid = String::new();
+            for i in 0..=9 {
+                for j in 0..=9 {
+                    // println!("displaying row {:?} column {:?}", i, j);
+                    match self.index.get(&Coordinate { row: j, column: i }) {
+                        Some(v) => grid.push_str(&v.to_string()),
+                        None => grid.push_str("."),
+                    }
+                }
+                grid.push_str("\n");
+            }
+            write!(f, "{}", grid)
         }
     }
 
@@ -148,13 +207,29 @@ fn main() {
 
     let mut grid = Grid::new();
 
-    // Fill Grid using segmentList
+    let mut part2_segment_list = segment_list.clone();
+
+    // Part 1 : Fill Grid using segmentList and NOT counting diagonal lines
     for segment in segment_list {
-        segment.points().into_iter().for_each(|p| {
+        segment.points(false).into_iter().for_each(|p| {
             grid.add_point(p);
         });
     }
 
     // Compute max occurrences
     println!("Part 1: Solution is {:?}", grid.dangerous_points_count());
+
+    // Part 2 : Fill Grid using segmentList and counting diagonal lines
+    let mut grid_part2 = Grid::new();
+
+    for segment in part2_segment_list {
+        segment.points(true).into_iter().for_each(|p| {
+            grid_part2.add_point(p);
+        });
+    }
+
+    println!(
+        "Part 2: Solution is {:?}",
+        grid_part2.dangerous_points_count()
+    );
 }
