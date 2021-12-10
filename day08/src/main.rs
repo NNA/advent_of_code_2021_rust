9f02b757aa084e476c8dcbc6edcb9da165b07981
Nicolas NARDONE <nna@protonmail.com>
2021-12-10 18:33:36 +0100

    combine part 1 & part2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -146,91 +146,94 @@ fn main() {
         notes.push(NoteEntry::from(line));
     }
 
-    // let notes_part2 = notes.clone();
-    // let mut easy_count = 0;
-    // notes.into_iter().for_each(|note_entry| {
-    //     easy_count += note_entry
-    //         .output_value
-    //         .iter()
-    //         .filter(|digit| digit.is_easy())
-    //         .count();
-    // });
+    let notes_part2 = notes.clone();
+    let mut easy_count = 0;
+    notes.into_iter().for_each(|note_entry| {
+        easy_count += note_entry
+            .output_value
+            .iter()
+            .filter(|digit| digit.is_easy())
+            .count();
+    });
 
-    // println!("Part 1: Solution {:?}", easy_count);
+    println!("Part 1: Solution {:?}", easy_count);
     let mut total = 0;
 
     //Part 2
-    notes.into_iter().enumerate().for_each(|(i, note_entry)| {
-        let mut deduced: HashMap<usize, &SignalPattern> = HashMap::new();
-        let mut decoded_digits: Vec<String> = vec![];
+    notes_part2
+        .into_iter()
+        .enumerate()
+        .for_each(|(i, note_entry)| {
+            let mut deduced: HashMap<usize, &SignalPattern> = HashMap::new();
+            let mut decoded_digits: Vec<String> = vec![];
 
-        // Identify digit 1, 7, 4 and 8 using unique length
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if let Some(digit) = length_to_digit(signal.active_wires.len()) {
-                deduced.insert(digit, signal);
-            }
-        });
+            // Identify digit 1, 7, 4 and 8 using unique length
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if let Some(digit) = length_to_digit(signal.active_wires.len()) {
+                    deduced.insert(digit, signal);
+                }
+            });
 
-        // Identify digits with length 6
+            // Identify digits with length 6
 
-        // If length is 6 and there is one char diff from signals for digit 1
-        // => Then it is digit 6
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 6
-                && deduced.get(&1).is_some()
-                && signal.is_missing_one_char_from(*deduced.get(&1).unwrap())
-            {
-                deduced.insert(6, signal);
-            }
-        });
+            // If length is 6 and there is one char diff from signals for digit 1
+            // => Then it is digit 6
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 6
+                    && deduced.get(&1).is_some()
+                    && signal.is_missing_one_char_from(*deduced.get(&1).unwrap())
+                {
+                    deduced.insert(6, signal);
+                }
+            });
 
-        // If length is 6 and there is one char diff from signals for digit 4
-        // and it's not 6 digit
-        // => Then it is digit 0
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 6
-                && deduced.get(&4).is_some()
-                && signal.is_missing_one_char_from(*deduced.get(&4).unwrap())
-                && (deduced
-                    .values()
-                    .filter(|s| s.to_string() == signal.to_string())
-                    .count()
-                    == 0)
-            // Because digit 6 is  1 char diff and we have already found it, let's ignore it
-            {
-                deduced.insert(0, signal);
-            }
-        });
+            // If length is 6 and there is one char diff from signals for digit 4
+            // and it's not 6 digit
+            // => Then it is digit 0
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 6
+                    && deduced.get(&4).is_some()
+                    && signal.is_missing_one_char_from(*deduced.get(&4).unwrap())
+                    && (deduced
+                        .values()
+                        .filter(|s| s.to_string() == signal.to_string())
+                        .count()
+                        == 0)
+                // Because digit 6 is  1 char diff and we have already found it, let's ignore it
+                {
+                    deduced.insert(0, signal);
+                }
+            });
 
-        // If length is 6 and it's not digit 6 or digit 0
-        // => Then it is digit 9
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 6
-                && (deduced
-                    .values()
-                    .filter(|s| s.to_string() == signal.to_string())
-                    .count()
-                    == 0)
-            {
-                deduced.insert(9, signal);
-            }
-        });
+            // If length is 6 and it's not digit 6 or digit 0
+            // => Then it is digit 9
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 6
+                    && (deduced
+                        .values()
+                        .filter(|s| s.to_string() == signal.to_string())
+                        .count()
+                        == 0)
+                {
+                    deduced.insert(9, signal);
+                }
+            });
 
-        // Identify digits with length 5
+            // Identify digits with length 5
 
-        // If length is 5 and same signals has digit 6 wires
-        // => Then it is digit 5
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 5
-                && signal.contains_all_wires_of(*deduced.get(&6).unwrap())
-            {
-                deduced.insert(5, signal);
-            }
-        });
+            // If length is 5 and same signals has digit 6 wires
+            // => Then it is digit 5
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 5
+                    && signal.contains_all_wires_of(*deduced.get(&6).unwrap())
+                {
+                    deduced.insert(5, signal);
+                }
+            });
 
-        // If length is 5 and same signals has digit 6 wires
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 5
+            // If length is 5 and same signals has digit 6 wires
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 5
                 && signal.contains_all_wires_of(*deduced.get(&9).unwrap())
                 // Because digit 5 is also same as 9 and we have already found it, let's ignore it
                 && (deduced
@@ -238,48 +241,48 @@ fn main() {
                     .filter(|s| s.to_string() == signal.to_string())
                     .count()
                     == 0)
-            {
-                deduced.insert(3, signal);
-            }
+                {
+                    deduced.insert(3, signal);
+                }
+            });
+
+            // If length is 5 and it's not digit 5 nor digit 3
+            // => Then it is digit 2
+            note_entry.signal_patterns.iter().for_each(|signal| {
+                if signal.active_wires.len() == 5
+                    && (deduced
+                        .values()
+                        .filter(|s| s.to_string() == signal.to_string())
+                        .count()
+                        == 0)
+                {
+                    deduced.insert(2, signal);
+                }
+            });
+
+            // Reverse the deduced hash : keys are now a sorted string of Signal Patterns.
+            let mut reverse: HashMap<String, String> = HashMap::new();
+            deduced.into_iter().for_each(|(k, v)| {
+                let mut chars: Vec<char> = v.active_wires.to_owned();
+                chars.sort();
+                let _res = reverse.insert(chars.iter().collect::<String>(), k.to_string());
+            });
+
+            // Seek patterns and decode the 4 digit in output
+            note_entry.output_value.iter().for_each(|digit| {
+                let mut sorted_digit: Vec<char> = digit.active_wires.to_owned();
+                sorted_digit.sort();
+
+                let pattern = sorted_digit.iter().collect::<String>();
+                let matching_digit: String = reverse.get(&pattern).unwrap().to_string();
+
+                decoded_digits.push(matching_digit);
+            });
+
+            // Append 4 digits into an integer add sum it
+            let output_value: String = decoded_digits.into_iter().collect();
+            total += output_value.parse::<usize>().unwrap();
         });
 
-        // If length is 5 and it's not digit 5 nor digit 3
-        // => Then it is digit 2
-        note_entry.signal_patterns.iter().for_each(|signal| {
-            if signal.active_wires.len() == 5
-                && (deduced
-                    .values()
-                    .filter(|s| s.to_string() == signal.to_string())
-                    .count()
-                    == 0)
-            {
-                deduced.insert(2, signal);
-            }
-        });
-
-        // Reverse the deduced hash : keys are now a sorted string of Signal Patterns.
-        let mut reverse: HashMap<String, String> = HashMap::new();
-        deduced.into_iter().for_each(|(k, v)| {
-            let mut chars: Vec<char> = v.active_wires.to_owned();
-            chars.sort();
-            let _res = reverse.insert(chars.iter().collect::<String>(), k.to_string());
-        });
-
-        // Seek patterns and decode the 4 digit in output
-        note_entry.output_value.iter().for_each(|digit| {
-            let mut sorted_digit: Vec<char> = digit.active_wires.to_owned();
-            sorted_digit.sort();
-
-            let pattern = sorted_digit.iter().collect::<String>();
-            let matching_digit: String = reverse.get(&pattern).unwrap().to_string();
-
-            decoded_digits.push(matching_digit);
-        });
-
-        // Append 4 digits into an integer add sum it
-        let output_value: String = decoded_digits.into_iter().collect();
-        total += output_value.parse::<usize>().unwrap();
-    });
-
-    println!("Part 2 solution is {:?}", total);
+    println!("Part 2: Solution {:?}", total);
 }
