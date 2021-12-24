10de396dd1b8959b3db9913b1032a297e7beab66
Nicolas NARDONE <nna@protonmail.com>
2021-12-24 17:41:04 +0100

    day12 part1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,11 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
+use std::ops::Index;
+use std::path::{self, Path};
+use std::{thread, time};
 
 fn main() {
     // Create a path to the desired file
-    let path = Path::new("input_test_small.txt");
+    let path = Path::new("input.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -21,42 +23,86 @@ fn main() {
         Ok(_) => (),
     }
 
-    #[derive(Debug)]
-    struct CavePap {
-        index: HashMap<String, Vec<String>>,
-    }
+    // #[derive(Debug)]
+    // struct CavePap {
+    //     index: HashMap<str, Vec<str>>,
+    // }
 
     type CaveMap = HashMap<String, Vec<String>>;
 
     let mut map: CaveMap = CaveMap::new();
 
     trait Segmentable {
-        fn add_segment(&mut self, start: String, end: String);
+        fn add_segment(&mut self, start: &str, end: &str);
+        fn traverse(&mut self, paths: Vec<Vec<String>>) -> Vec<Vec<String>>;
     }
 
     impl Segmentable for CaveMap {
-        fn add_segment(&mut self, start: String, end: String) {
-            match self.get_mut(&start) {
-                Some(existing) => {
-                    existing.push(end.to_string());
+        fn add_segment(&mut self, start: &str, end: &str) {
+            self.entry(start.to_string())
+                .or_default()
+                .push(end.to_string());
+            self.entry(end.to_string())
+                .or_default()
+                .push(start.to_string());
+        }
+
+        fn traverse(&mut self, paths: Vec<Vec<String>>) -> Vec<Vec<String>> {
+            // println!("******** NEW traverse");
+            // println!("paths {:?}", paths);
+            let mut cloned_paths = paths.clone();
+            let mut added = false;
+            let mut new_paths: Vec<Vec<String>> = vec![];
+
+            while let Some(path) = cloned_paths.pop() {
+                // println!("poping path {:?}", path);
+                let last_element = path.last().unwrap();
+                if *last_element == "end".to_string() {
+                    new_paths.push(path);
+                    continue;
                 }
-                None => {
-                    self.insert(start.to_string(), vec![end.to_string()]);
+                match self.get_mut(last_element) {
+                    Some(neighborlist) => {
+                        let base_path = path.clone();
+                        neighborlist.iter().for_each(|neighbor| {
+                            let visited = path.contains(neighbor);
+                            let allowed_multiple = *neighbor == neighbor.to_uppercase();
+                            if (allowed_multiple || !visited) {
+                                let mut cloned = base_path.clone();
+                                cloned.push(neighbor.clone());
+                                // println!("new ongoing path {:?}", cloned);
+                                new_paths.push(cloned);
+                                added = true;
+                            }
+                        });
+                    }
+                    None => (),
                 }
+            }
+
+            // println!("new_paths {:?}", new_paths);
+            if added {
+                // At least one path has increased, we need to try to increase it.
+                self.traverse(new_paths)
+            } else {
+                // No paths added, we cannot continue => we only ouput those that end at "end"
+                paths
+                    .into_iter()
+                    .filter(|r| *r.last().unwrap() == "end".to_string())
+                    .collect()
             }
         }
     }
 
+    // Build the map by adding segments
     for segment in content.lines() {
         let mut iter = segment.split('-');
-        let start = iter.next().unwrap();
-        let end = iter.next().unwrap();
-
-        map.add_segment(start.to_string(), end.to_string());
-        if end.to_string() == end.to_ascii_uppercase() {
-            map.add_segment(end.to_string(), start.to_string());
-        }
+        map.add_segment(iter.next().unwrap(), iter.next().unwrap());
     }
 
-    println!("map {:?}", map);
+    // Traverse recursively
+    let res = map.traverse(vec![vec!["start".to_string()]]);
+
+    // println!("res {:?}", res);
+    println!("Part1 : Solution is {:?}", res.len());
 }
