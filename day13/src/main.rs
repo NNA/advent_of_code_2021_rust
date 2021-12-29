0b9a2b4e622b357162093ba44cdb8d6f1b579ffc
Nicolas NARDONE <nna@protonmail.com>
2021-12-29 18:36:53 +0100

    day13 part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,17 +1,17 @@
-use std::borrow::BorrowMut;
+use core::fmt;
 use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 struct Coordinate {
     x: u16,
     y: u16,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Direction {
     X,
     Y,
@@ -19,9 +19,9 @@ enum Direction {
 
 type Position = u16;
 type Fold = (Direction, Position);
-type PaperIndex = Vec<Coordinate>;
+type PaperIndex = HashSet<Coordinate>;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct Paper {
     index: PaperIndex,
     foldings: Vec<Fold>,
@@ -37,16 +37,15 @@ impl Paper {
         let foldings_str = split.next().unwrap();
 
         for coord in coords_str.lines() {
-            let mut iter = coord.split(",");
-            index.push(Coordinate {
-                x: iter.next().unwrap().to_string().parse().unwrap(),
-                y: iter.next().unwrap().to_string().parse().unwrap(),
-            });
+            let mut iter = coord.split(',');
+            let x = iter.next().unwrap().to_string().parse().unwrap();
+            let y = iter.next().unwrap().to_string().parse().unwrap();
+            index.insert(Coordinate { x, y });
         }
 
         for fold in foldings_str.lines() {
-            let split = fold.split(" ");
-            let mut iter = split.last().unwrap().split("=");
+            let split = fold.split(' ');
+            let mut iter = split.last().unwrap().split('=');
             let direction = match iter.next() {
                 Some("x") => Direction::X,
                 Some("y") => Direction::Y,
@@ -55,60 +54,76 @@ impl Paper {
             foldings.push((direction, iter.next().unwrap().parse().unwrap()));
         }
 
-        // So we can pop() all foldings
-        foldings.reverse();
-
-        Paper {
-            index: index,
-            foldings: foldings,
-        }
+        Paper { index, foldings }
     }
 
-    fn fold_once(&mut self) {
-        if let Some((direction, position)) = self.foldings.pop() {
-            let mut folded = vec![];
-            while let Some(coord) = self.index.pop() {
-                match direction {
-                    Direction::X => match coord.x.partial_cmp(&position).unwrap() {
-                        Ordering::Greater => {
-                            folded.push(Coordinate {
-                                x: position - (coord.x - position),
-                                y: coord.y,
-                            });
-                        }
-                        Ordering::Less => {
-                            folded.push(Coordinate {
-                                x: coord.x,
-                                y: coord.y,
-                            });
-                        }
-                        Ordering::Equal => (),
-                    },
-                    Direction::Y => match coord.y.partial_cmp(&position).unwrap() {
-                        Ordering::Greater => {
-                            folded.push(Coordinate {
-                                x: coord.x,
-                                y: position - (coord.y - position),
-                            });
-                        }
-                        Ordering::Less => {
-                            folded.push(Coordinate {
-                                x: coord.x,
-                                y: coord.y,
-                            });
-                        }
-                        Ordering::Equal => (),
-                    },
-                };
+    fn fold_once_at(&mut self, direction: Direction, position: Position) {
+        let mut folded = HashSet::new();
+        self.index.iter().for_each(|coord| {
+            match direction {
+                Direction::X => match coord.x.partial_cmp(&position).unwrap() {
+                    Ordering::Greater => {
+                        folded.insert(Coordinate {
+                            x: position - (coord.x - position),
+                            y: coord.y,
+                        });
+                    }
+                    Ordering::Less => {
+                        folded.insert(Coordinate {
+                            x: coord.x,
+                            y: coord.y,
+                        });
+                    }
+                    Ordering::Equal => (),
+                },
+                Direction::Y => match coord.y.partial_cmp(&position).unwrap() {
+                    Ordering::Greater => {
+                        folded.insert(Coordinate {
+                            x: coord.x,
+                            y: position - (coord.y - position),
+                        });
+                    }
+                    Ordering::Less => {
+                        folded.insert(Coordinate {
+                            x: coord.x,
+                            y: coord.y,
+                        });
+                    }
+                    Ordering::Equal => (),
+                },
+            };
+        });
+        self.index = folded;
+    }
+
+    fn fold(&mut self, limit: Option<u16>) {
+        for (count, (direction, position)) in self.foldings.clone().into_iter().enumerate() {
+            self.fold_once_at(direction, position);
+            if limit.is_some() && count == (limit.unwrap() - 1).into() {
+                break;
             }
-            folded.sort();
-            folded.dedup();
-            self.index = folded;
         }
     }
 
     fn visible_dot_count(self) -> usize {
         self.index.len()
+    }
+}
+
+impl fmt::Display for Paper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut grid = String::new();
+        for i in 0..=6 {
+            for j in 0..=40 {
+                // println!("displaying row {:?} column {:?}", i, j);
+                match self.index.get(&Coordinate { x: j, y: i }) {
+                    Some(_v) => grid.push('#'),
+                    None => grid.push('.'),
+                }
+            }
+            grid.push('\n');
+        }
+        write!(f, "{}", grid)
     }
 }
 
@@ -125,19 +140,26 @@ fn main() {
 
     // Read the file contents into a string, returns `io::Result<usize>`
     let mut content = String::new();
-    match file.read_to_string(&mut content) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => (),
+    if let Err(why) = file.read_to_string(&mut content) {
+        panic!("couldn't read {}: {}", display, why)
     }
 
+    // Part 1
     let mut paper: Paper = Paper::new(content);
-    paper.fold_once();
-    let result = paper.visible_dot_count();
-    println!("Part1 result is {:?}", result);
+    let mut paper_part2 = paper.clone();
+    paper.fold(Some(1));
+    let sol1 = paper.visible_dot_count();
+    println!("Part1 solution is {:?}", sol1);
+
+    // Part 2
+    paper_part2.fold(None);
+    println!("Part2 solution is \n{:}", paper_part2);
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use crate::{Coordinate, Direction, Paper};
     // use std::collections::HashSet;
 
@@ -150,39 +172,28 @@ fold along y=7"#;
     fn it_can_properly_initialize_a_paper() {
         let expected_paper: Paper = Paper::new(SIMPLE_CONTENT.to_string());
 
-        let mut i = vec![];
-        i.push(Coordinate { x: 6, y: 10 });
-        i.push(Coordinate { x: 0, y: 14 });
+        let mut index = HashSet::new();
+        index.insert(Coordinate { x: 6, y: 10 });
+        index.insert(Coordinate { x: 0, y: 14 });
 
-        let mut f = vec![];
-        f.push((Direction::Y, 7));
+        let mut foldings = vec![];
+        foldings.push((Direction::Y, 7));
 
-        assert_eq!(
-            expected_paper,
-            Paper {
-                index: i,
-                foldings: f
-            }
-        );
+        assert_eq!(expected_paper, Paper { index, foldings });
     }
 
     #[test]
     fn it_can_fold() {
         let mut expected_paper: Paper = Paper::new(SIMPLE_CONTENT.to_string());
-        expected_paper.fold();
+        expected_paper.fold(Some(1));
 
-        let mut i = vec![];
-        i.push(Coordinate { x: 0, y: 0 });
-        i.push(Coordinate { x: 6, y: 4 });
+        let mut index = HashSet::new();
+        index.insert(Coordinate { x: 0, y: 0 });
+        index.insert(Coordinate { x: 6, y: 4 });
 
-        let f = vec![];
+        let mut foldings = vec![];
+        foldings.push((Direction::Y, 7));
 
-        assert_eq!(
-            expected_paper,
-            Paper {
-                index: i,
-                foldings: f
-            }
-        );
+        assert_eq!(expected_paper, Paper { index, foldings });
     }
 }
